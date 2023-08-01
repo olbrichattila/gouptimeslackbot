244faa902c5d81bdfde78c03892ec82ce1cb8382
Attila Olbrich <olbrichattila@gmx.co.uk>
2023-08-01 22:38:35 +0100

    Refactor code to be more testable and structured

--- main.go
+++ main.go
@@ -11,13 +11,18 @@ type app struct {
 	client         upClientInterface
 	slackPublisher SlackPublisherInterface
 	config         configInterface
+	scanner        scannerInterface
 }
 
 func newApp() *app {
+	client := newUpClient(&request{})
+	publisher := NewSlackPublisher()
+
 	return &app{
-		client:         newUpClient(&request{}),
-		slackPublisher: NewSlackPublisher(),
+		client:         client,
+		slackPublisher: publisher,
 		config:         resolveConfig(),
+		scanner:        newScanner(client, publisher),
 	}
 }
 
@@ -28,35 +33,9 @@ func main() {
 	accounts := app.config.getConfigAccounts()
 	for {
 		for _, config := range *accounts {
-			go doScan(app, config)
+			app.scanner.Scan(config)
 		}
 		time.Sleep(time.Duration(frequency) * time.Second)
-	}
-}
-
-/*
- * @todo move this out from here into a tetable format
- * add http response code check and report other then 20*
- * limit number of messages if there are too many and aggregate them
- */
-func doScan(app *app, config configAccount) {
-	elapsed, err := app.client.TestUrl(config.MonitorUrl, config.MonitorText)
-	if err != nil {
-		// @todo add event date / time to the message
-		message := fmt.Sprintf("%s:\nUp boat report:\n\tElapsed: %d\n\tError:%v", config.MonitorUrl, elapsed, err)
-		err = app.slackPublisher.Send(config.SlackBotToken, config.SlackChannelId, message)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	if config.SlowWarningLimit > 0 && elapsed > config.SlowWarningLimit {
-		// @todo add event date / time to the message
-		message := fmt.Sprintf("%s:\nSlow warning limit reached:\n\tLimit: %d\n\tElapsed: %d", config.MonitorUrl, config.SlowWarningLimit, elapsed)
-		err = app.slackPublisher.Send(config.SlackBotToken, config.SlackChannelId, message)
-		if err != nil {
-			fmt.Println(err)
-		}
 	}
 }
 
---
+++ scan.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type scannerInterface interface {
+	Scan(config configAccount)
+}
+
+type scanner struct {
+	client    upClientInterface
+	publisher SlackPublisherInterface
+}
+
+func newScanner(client upClientInterface, publisher SlackPublisherInterface) *scanner {
+	s := &scanner{}
+	s.client = client
+	s.publisher = publisher
+
+	return s
+}
+
+func (s *scanner) Scan(config configAccount) {
+	go s.doScan(config)
+}
+
+func (s *scanner) doScan(config configAccount) {
+	formattedDateTime := time.Now().Format("2006-01-02 15:04:05")
+	elapsed, err := s.client.TestUrl(config.MonitorUrl, config.MonitorText)
+	if err != nil {
+		message := fmt.Sprintf(
+			"Host: %s:\nUp bot report:\n\tDate: %s\n\tElapsed: %d miliseconds\n\tError:%v",
+			config.MonitorUrl,
+			formattedDateTime,
+			elapsed,
+			err,
+		)
+		err = s.publisher.Send(config.SlackBotToken, config.SlackChannelId, message)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	if config.SlowWarningLimit > 0 && elapsed > config.SlowWarningLimit {
+		message := fmt.Sprintf(
+			"Host: %s:\nSlow warning limit reached:\n\tDate: %s\n\tLimit: %d miliseconds\n\tElapsed: %d miliseconds",
+			config.MonitorUrl,
+			formattedDateTime,
+			config.SlowWarningLimit,
+			elapsed,
+		)
+		err = s.publisher.Send(config.SlackBotToken, config.SlackChannelId, message)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
